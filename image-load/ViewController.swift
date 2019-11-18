3545b3e76d1daac2a52c906d8c83ec8079d7aa15
Artem Ulko <ulkoart@gmail.com>
2019-11-18 08:58:14 +0300

    Initial Commit

---
+++ image-load/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image-load
+//
+//  Created by user on 18/11/2019.
+//  Copyright © 2019 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
